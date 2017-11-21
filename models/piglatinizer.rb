ebe866d3f07bd3d2279173ef5011482909a9c380
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-21 02:27:03 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,29 @@
+class PigLatinizer
+
+  attr_accessor :text, :converted_text
+
+  vowels = "aeiou"
+
+  def to_pig_latin(some_text)
+    @text = some_text.split(" ")
+    piglatinize(@text)
+  end
+
+  def piglatinize(text_to_convert)
+    if(@text.count == 1)
+      if(vowels.contains(@text[0]))
+        @converted_text = @text + "way"
+      end
+    else
+      @text.each do |word|
+        if(vowels.contains(word[0]))
+          @converted_text += word + "way"
+        elsif(vowels.contains(word[1]))
+          @converted_text += word.split.rotate(1).join + "ay "
+        else
+          @converted_text += word.split.rotate(2).join + "ay "
+        end
+      end
+    end
+  end
+end
