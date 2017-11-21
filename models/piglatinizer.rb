ab4fd73186e2994b5dfb4eb93522081a1838472a
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-21 02:34:38 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,18 +14,18 @@ class PigLatinizer
       if(vowels.include?(text_to_convert[0]))
         @converted_text = text_to_convert + "way"
       elsif(vowels.include?(text_to_convert[1]))
-        @converted_text = text_to_convert.split.rotate(1).join + "ay "
+        @converted_text = text_to_convert.split("").rotate(1).join + "ay "
       else
-        @converted_text = text_to_convert.split.rotate(2).join + "ay "
+        @converted_text = text_to_convert.split("").rotate(2).join + "ay "
       end
     else
       text_to_convert.each do |word|
         if(vowels.include?(word[0]))
           @converted_text += word + "way"
         elsif(vowels.include?(word[1]))
-          @converted_text += word.split.rotate(1).join + "ay "
+          @converted_text += word.split("").rotate(1).join + "ay "
         else
-          @converted_text += word.split.rotate(2).join + "ay "
+          @converted_text += word.split("").rotate(2).join + "ay "
         end
       end
     end
