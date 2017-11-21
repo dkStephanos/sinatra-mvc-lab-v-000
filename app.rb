f827ff63811796a28010b682bd8bd0ff26ded6ed
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-21 02:47:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     piglatinizer = PigLatinizer.new
-    @output = piglatinizer.to_pig_latin(@user_input)
+    #@output = piglatinizer.to_pig_latin(@user_input)
 
     erb :piglatinized
   end
